60dcf6b9210eb66cce0becf54956f609a9403c43
Eric Garside <garside@gmail.com>
2019-09-11 11:59:54 -0400

    Attributes

--- lib/material.rb
+++ lib/material.rb
@@ -16,6 +16,7 @@ require_relative "material/concerns/pagination"
 require_relative "material/concerns/mount"
 require_relative "material/concerns/collection"
 require_relative "material/concerns/format"
+require_relative "material/concerns/attributes"
 
 require "material/version"
 
---
+++ lib/material/concerns/attributes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Material
+  module Attributes
+    extend ActiveSupport::Concern
+
+  end
+end
