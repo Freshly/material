7d739a892fb6204deb61349ee3b496cb45fa107b
Eric Garside <garside@gmail.com>
2019-09-11 08:46:23 -0400

    Display Refactor

--- lib/material/list.rb
+++ lib/material/list.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "concerns/components"
-require_relative "concerns/icon"
-require_relative "concerns/text"
-
 module Material
   class List < Collectible::CollectionBase
     include Material::Components
