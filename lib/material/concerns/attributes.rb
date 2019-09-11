137da6841af63609fbd294b97e4d11790ea8c43e
Eric Garside <garside@gmail.com>
2019-09-11 14:02:23 -0400

    Attributes

--- lib/material/concerns/attributes.rb
+++ lib/material/concerns/attributes.rb
@@ -5,15 +5,53 @@ module Material
     extend ActiveSupport::Concern
 
     included do
+      delegate :head_attributes, :tail_attributes, to: :class
+
+      memoize :attribute_values
+      memoize :attribute_types
       memoize :formatted_attributes
+      memoize :sorted_attribute_names
+      memoize :relationship_attributes
+    end
+
+    class_methods do
+      def head_attributes
+        %w[id]
+      end
+
+      def tail_attributes
+        %w[created_at updated_at]
+      end
+    end
+
+    def human_attribute_value(attribute)
+      formatted_attributes.fetch(attribute)
+    end
+
+    def attribute_values
+      attribute_names.each_with_object({}) { |attribute, hash| hash[attribute] = public_send(attribute.to_sym) }
+    end
+
+    def attribute_types
+      attribute_names.each_with_object({}) { |attribute, hash| hash[attribute] = type_for_attribute(attribute) }
     end
 
     def formatted_attributes
-      attribute_names.each_with_object({}) do |attribute_name, hash|
-        attribute_type = type_for_attribute(attribute_name)
-        attribute_value = public_send(attribute_name.to_sym)
-        hash[human_attribute_name(attribute_name)] = format_by_type(attribute_value, type: attribute_type)
+      attribute_types.each_with_object({}) do |(attribute, attribute_type), hash|
+        hash[attribute] = format_by_type(attribute_values[attribute], type: attribute_type)
       end
+    end
+
+    def sorted_attribute_names
+      [
+        (head_attributes & attribute_names),
+        (attribute_names - head_attributes - tail_attributes).sort,
+        (tail_attributes & attribute_names),
+      ].flatten
+    end
+
+    def relationship_attributes
+      source_class.reflect_on_all_associations.map(&:foreign_key)
     end
   end
 end
--- spec/material/attributes_spec.rb
+++ spec/material/attributes_spec.rb
@@ -3,7 +3,151 @@
 RSpec.describe Material::Attributes, type: :concern do
   include_context "with an example material"
 
+  it { is_expected.to delegate_method(:head_attributes).to(:class) }
+  it { is_expected.to delegate_method(:tail_attributes).to(:class) }
+
+  describe ".head_attributes" do
+    subject { example_material_class.head_attributes }
+
+    it { is_expected.to eq %w[id] }
+  end
+
+  describe ".tail_attributes" do
+    subject { example_material_class.tail_attributes }
+
+    it { is_expected.to eq %w[created_at updated_at] }
+  end
+
+  describe "#human_attribute_value" do
+    subject(:human_attribute_value) { example_material.human_attribute_value(attribute) }
+
+    let(:formatted_attributes) do
+      { attribute_name => attribute_value }
+    end
+
+    let(:attribute_name) { Faker::Internet.domain_word }
+    let(:attribute_value) { SecureRandom.hex }
+
+    before { allow(example_material).to receive(:formatted_attributes).and_return(formatted_attributes) }
+
+    context "when present" do
+      let(:attribute) { attribute_name }
+
+      it { is_expected.to eq attribute_value }
+    end
+
+    context "when absent" do
+      let(:attribute) { SecureRandom.hex }
+
+      it "raises" do
+        expect { human_attribute_value }.to raise_error KeyError, "key not found: \"#{attribute}\""
+      end
+    end
+  end
+
+  describe "#attribute_values" do
+    subject { example_material.attribute_values }
+
+    let(:attribute_names) { [ attribute0, attribute1 ] }
+
+    let(:attribute0) { Faker::Internet.domain_word }
+    let(:value0) { SecureRandom.hex }
+    let(:attribute1) { "#{attribute0}foo" }
+    let(:value1) { SecureRandom.hex }
+
+    let(:expected_hash) do
+      { attribute0 => value0, attribute1 => value1 }
+    end
+
+    before do
+      allow(example_material).to receive(:attribute_names).and_return(attribute_names)
+      allow(example_material).to receive(attribute0.to_sym).and_return(value0)
+      allow(example_material).to receive(attribute1.to_sym).and_return(value1)
+    end
+
+    it { is_expected.to eq expected_hash }
+  end
+
+  describe "#attribute_types" do
+    subject { example_material.attribute_types }
+
+    let(:attribute_names) { [ attribute0, attribute1 ] }
+
+    let(:attribute0) { Faker::Internet.domain_word }
+    let(:type0) { SecureRandom.hex }
+    let(:attribute1) { "#{attribute0}foo" }
+    let(:type1) { SecureRandom.hex }
+
+    let(:expected_hash) do
+      { attribute0 => type0, attribute1 => type1 }
+    end
+
+    before do
+      allow(example_material).to receive(:attribute_names).and_return(attribute_names)
+      allow(example_material).to receive(:type_for_attribute).with(attribute0).and_return(type0)
+      allow(example_material).to receive(:type_for_attribute).with(attribute1).and_return(type1)
+    end
+
+    it { is_expected.to eq expected_hash }
+  end
+
   describe "#formatted_attributes" do
-    it "needs specs"
+    subject { example_material.formatted_attributes }
+
+    let(:attribute_types) do
+      { attribute0 => type0, attribute1 => type1 }
+    end
+
+    let(:attribute_values) do
+      { attribute0 => value0, attribute1 => value1 }
+    end
+
+    let(:attribute0) { Faker::Internet.domain_word }
+    let(:type0) { SecureRandom.hex }
+    let(:value0) { SecureRandom.hex }
+    let(:formatted0) { "pretty: #{value0}" }
+
+    let(:attribute1) { "#{attribute0}foo" }
+    let(:type1) { SecureRandom.hex }
+    let(:value1) { SecureRandom.hex }
+    let(:formatted1) { "pretty: #{value1}" }
+
+    let(:expected_hash) do
+      { attribute0 => formatted0, attribute1 => formatted1 }
+    end
+
+    before do
+      allow(example_material).to receive(:attribute_types).and_return(attribute_types)
+      allow(example_material).to receive(:attribute_values).and_return(attribute_values)
+      allow(example_material).to receive(:format_by_type).with(value0, type: type0).and_return(formatted0)
+      allow(example_material).to receive(:format_by_type).with(value1, type: type1).and_return(formatted1)
+    end
+
+    it { is_expected.to eq expected_hash }
+  end
+
+  describe "#sorted_attribute_names" do
+    subject { example_material.sorted_attribute_names }
+
+    let(:attribute_names) { %w[dog cat id updated_at apple created_at banana] }
+
+    before { allow(example_material).to receive(:attribute_names).and_return(attribute_names) }
+
+    it { is_expected.to eq %w[id apple banana cat dog created_at updated_at] }
+  end
+
+  describe "#relationship_attributes" do
+    subject { example_material.relationship_attributes }
+
+    let(:source_class) { example_material.source_class }
+    let(:foreign_key0) { Faker::Internet.domain_word }
+    let(:foreign_key1) { "#{foreign_key0}foo" }
+    let(:reflect_on_all_associations) do
+      [ double(foreign_key: foreign_key0), double(foreign_key: foreign_key1) ]
+    end
+
+    before { allow(source_class).to receive(:reflect_on_all_associations).and_return(reflect_on_all_associations) }
+
+    it { is_expected.to eq [ foreign_key0, foreign_key1 ] }
   end
 end
